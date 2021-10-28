3ea3707002b77fe681bd59a592d85b66e4dab22b
JiaWen02 <tanjw-jm20@student.tarc.edu.my>
2021-10-28 17:51:59 +0800

    Third Commit - Changes in CSS

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -52,6 +52,47 @@
     </div>
   </div>
 
+  <!-- First Photo Grid-->
+<div class="w3-row-padding w3-padding-16 w3-center" id="college" style="margin-top: 50px;">
+    <h1 class="w3-center">Healing Sunrise</h1><br>
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise2.jpg')}}" alt="hong kong" style="width:100%">
+      <h4>Hong Kong</h4>
+    </div>
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise3.jpg')}}" alt="taiwan" style="width:100%">
+      <h4>Taiwan</h4>
+    </div>
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise4.jpg')}}" alt="us state" style="width:100%">
+      <h4>US State</h4>
+    </div>
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise5.jpg')}}" alt="japan" style="width:100%">
+      <h4>Japan</h4>
+    </div>
+  </div>
+  
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise6.jpg')}}" alt="redang" style="width:100%">
+      <h4>Redang Island</h4>
+    </div>
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise7.jpg')}}" alt="seoul" style="width:100%">
+      <h4>Seoul</h4>
+    </div>
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise8.jpg')}}" alt="new zealand" style="width:100%; height:170px;">
+      <h4>New Zealand</h4>
+    </div>
+    <div class="w3-quarter">
+      <img src="{{url('/images/sunrise9.jpg')}}" alt="australia" style="width:100%; height:170px;">
+      <h4>Australia</h4>
+    </div>
+  </div>
+
 <!-- End page content -->
 </div><br><br><br><br>
 
