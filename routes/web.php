ba00f2771bb47aa2826f7a742838659c2bab94dc
JiaWen02 <92565261+JiaWen02@users.noreply.github.com>
2021-11-03 17:19:30 +0800

    Added update & delete functions

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Models\Register;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DeleteController;
+use App\Http\Controllers\UpdateController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +34,6 @@ Route::get('/register', 'App\Http\Controllers\RegisterController@insertform');
 Route::post('/create', 'App\Http\Controllers\RegisterController@insert');
 Route::get('/login', 'App\Http\Controllers\LoginController@index');
 Route::post('/check', 'App\Http\Controllers\LoginController@checkLogin');
+Route::any('/delete', 'App\Http\Controllers\DeleteController@delete');
+Route::any('/updatepage', 'App\Http\Controllers\UpdateController@updateform');
+Route::any('/update', 'App\Http\Controllers\UpdateController@update');
